c0da39bf5defea4ba3dc5c5f4f67824cc8a6ef82
AldoVieiraPinto <aldo.vieira@ifmg.edu.br>
2024-10-30 14:23:00 -0300

    Update GeraCenarioLow.cpp

--- Instâncias/Códigos Geração de Cenários/GeraCenarioLow.cpp
+++ Instâncias/Códigos Geração de Cenários/GeraCenarioLow.cpp
@@ -23,18 +23,18 @@ int main(int argc, char* argv[]) {
 			numcen = atoi(argv[2]);
 		int n; // número de nós lido da instância
 		arq >> n;
-		vector<vector<double > > w(n, vector<double>(n)); // declara um vetor de vetores para representar uma matriz com n linhas e n colunas - Quantidade de demanda a ser enviada entre os nós i e j
 		vector<vector<double > > c(n, vector<double>(n)); // Custos por enviar uma unidade de demanda entre os nós i e j
-		vector<vector<double > > watual(n, vector<double>(n)); // declara um vetor de vetores para representar uma matriz com n linhas e n colunas - Quantidade de demanda a ser enviada entre os nós i e j no cenário atual
-		vector<vector<double > > catual(n, vector<double>(n)); // Custos por enviar uma unidade de demanda entre os nós i e j no cenário atual
+		vector<vector<double > > w(n, vector<double>(n)); // Quantidade de demanda a ser enviada entre os nós i e j
+		vector<vector<double > > catual(n, vector<double>(n)); //  Custos por enviar uma unidade de demanda entre os nós i e j no cenário atual
+		vector<vector<double > > watual(n, vector<double>(n)); // Quantidade de demanda a ser enviada entre os nós i e j no cenário atual
 
-		for (int i = 0; i < n; i++) {   //Coletar fluxos 
+		for (int i = 0; i < n; i++) {   //Coletar custos 
 			for (int j = 0; j < n; j++) {
 				arq >> c[i][j];
 			}
 		}
 
-		for (int i = 0; i < n; i++) {   //Coletar custos
+		for (int i = 0; i < n; i++) {   //Coletar fluxos
 			for (int j = 0; j < n; j++) {
 				arq >> w[i][j];
 			}
@@ -43,37 +43,37 @@ int main(int argc, char* argv[]) {
 		std::random_device rd;
 		std::mt19937 gen(rd());
 
-		// Define o intervalo desejado - Low
+		// Define o intervalo desejado - High
 		double min = 0.9;
 		double max = 1.1;
 		FILE* re;
-		re = fopen("CAB-Low10.txt", "w+"); //exemplo de saída para CAB com 10 cenários
+		re = fopen("CAB-High10.txt", "w+"); //exemplo de saída para CAB com 10 cenários
 
 		
 		std::uniform_real_distribution<double> dis(min, max);
 
 
 		for (int m = 0; m < numcen; m++) {
-			for (int i = 0; i < n; i++) { 
+			for (int i = 0; i < n; i++) { //Cálculo do vetor o: Matriz com a soma de demanadas a partir de cada nó
 				for (int j = 0; j < n; j++) {
 					catual[i][j] = dis(gen) * c[i][j];
 				}
 			}
 
-			for (int i = 0; i < n; i++) { 
+			for (int i = 0; i < n; i++) { //Cálculo do vetor o: Matriz com a soma de demanadas a partir de cada nó
 				for (int j = 0; j < n; j++) {
 					watual[i][j] = dis(gen) * w[i][j];
 				}
 			}
 
 
-			for (int i = 0; i < 25; i++) { 
+			for (int i = 0; i < 25; i++) { //Cálculo do vetor o: Matriz com a soma de demanadas a partir de cada nó
 				for (int j = 0; j < 25; j++) {
 					fprintf(re, "%10.5f \t ", catual[i][j]);
 				}
 				fprintf(re, "\n");
 			}
-			for (int i = 0; i < 25; i++) { 
+			for (int i = 0; i < 25; i++) { //Cálculo do vetor o: Matriz com a soma de demanadas a partir de cada nó
 				for (int j = 0; j < 25; j++) {
 					fprintf(re, "%10.5f \t ", watual[i][j]);
 				}
